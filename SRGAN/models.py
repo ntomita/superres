6cfc06907d5f25cf5026c70930704dbd82395d66
TomitaNaofumi <Naofumi.Tomita.GR@dartmouth.edu>
2017-03-19 00:13:09 -0400

    SRGAN first commit

--- SRGAN/models.py
+++ SRGAN/models.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
 from torch import autograd
-#from torch.autograd import Variable
 from torch.nn.init import xavier_normal, constant
 from torchvision import models
+
 
 class ResBlock(nn.Module):
     def __init__(self, n=64, s=1, f=3):
@@ -49,7 +49,6 @@ class DeconvBlock(nn.Module):
 
 
 class GenNet(nn.Module):
-
     def __init__(self, net_opts):
         # net_opts not used now
         super().__init__()
@@ -71,14 +70,18 @@ class GenNet(nn.Module):
         self.res13 = ResBlock()
         self.res14 = ResBlock()
         self.res15 = ResBlock()
+        self.conv2 = nn.Conv2d(64, 64, 3, 1, 1)
+        xavier_normal(self.conv2.weight)
+        self.bn = nn.BatchNorm2d(64)
         self.deconv1 = DeconvBlock()
         self.deconv2 = DeconvBlock()
-        self.conv2 = nn.Conv2d(64, 3, 3, 1, 1)
-        xavier_normal(self.conv2.weight)
+        self.conv3 = nn.Conv2d(64, 3, 3, 1, 1)
+        xavier_normal(self.conv3.weight)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.res1(x)
+        xs = self.relu(self.conv1(x))
+
+        x = self.res1(xs)
         x = self.res2(x)
         x = self.res3(x)
         x = self.res4(x)
@@ -93,9 +96,11 @@ class GenNet(nn.Module):
         x = self.res13(x)
         x = self.res14(x)
         x = self.res15(x)
+        x = self.bn(self.conv2(x))
+        x = x + xs
         x = self.relu(self.deconv1(x))
         x = self.relu(self.deconv2(x))
-        x = self.conv2(x)
+        x = self.conv3(x)
         return x
 
 
@@ -113,9 +118,86 @@ def vgg13_52():
     model.classifier = Skip()
     return model
 
+
+netspec_opts = dict()
+netspec_opts['input_channels'] = 3
+netspec_opts['layer_type'] = ['conv', 'lrelu',
+                              'conv', 'lrelu', 'bn',
+                              'conv', 'lrelu', 'bn',
+                              'conv', 'lrelu', 'bn',
+                              'conv', 'lrelu', 'bn',
+                              'conv', 'lrelu', 'bn',
+                              'conv', 'lrelu', 'bn',
+                              'conv', 'lrelu', 'bn']
+netspec_opts['num_filters'] = [64, 0,
+                               64, 0, 64,
+                               128, 0, 128,
+                               128, 0, 128,
+                               256, 0, 256,
+                               256, 0, 256,
+                               512, 0, 512,
+                               512, 0, 512]
+netspec_opts['kernel_size'] = [3, 0,
+                               3, 0, 0,
+                               3, 0, 0,
+                               3, 0, 0,
+                               3, 0, 0,
+                               3, 0, 0,
+                               3, 0, 0,
+                               3, 0, 0]
+netspec_opts['stride'] = [1, 0,
+                          2, 0, 0,
+                          1, 0, 0,
+                          2, 0, 0,
+                          1, 0, 0,
+                          2, 0, 0,
+                          1, 0, 0,
+                          2, 0, 0]
+
+
+def make_layers(nopts):
+    n = len(nopts['layer_type'])
+    layers = []
+    prev_filters = nopts['input_channels']
+    for i in range(n):
+        if nopts['layer_type'][i] == 'conv':
+            curr_filters = nopts['num_filters'][i]
+            layers.append(nn.Conv2d(
+                prev_filters,
+                curr_filters,
+                nopts['kernel_size'][i],
+                nopts['stride'][i],
+                (nopts['kernel_size'][i]-1)//2))
+            prev_filters = curr_filters
+        elif nopts['layer_type'][i] == 'lrelu':
+            layers.append(nn.LeakyReLU())
+        elif nopts['layer_type'][i] == 'bn':
+            curr_filters = nopts['num_filters'][i]
+            layers.append(nn.BatchNorm2d(curr_filters))
+            prev_filters = curr_filters
+    return nn.Sequential(*layers)
+
+
 class DisNet(nn.Module):
     def __init__(self):
-        pass
-    def forward(self):
-        pass
-        
+        super().__init__()
+        self.features = make_layers(netspec_opts)
+        self.classifier = nn.Sequential(
+            nn.Linear(6 * 6 * 512, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 1),
+            nn.Sigmoid())
+        self._init_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _init_weights(self):
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                xavier_normal(module.weight)
+
+
--- SRGAN/train.py
+++ SRGAN/train.py
@@ -8,7 +8,7 @@ from os import makedirs
 import argparse
 import re
 
-from models import GenNet
+from models import GenNet, DisNet, vgg13_52
 from dataset import get_train_set, get_val_set
 
 import sys
@@ -17,9 +17,11 @@ from data import download_bsds300
 from utils.plot import Plots
 from utils.utils import psnr
 
+
 def train(argv=sys.argv[1:]):
     upscale_factor = 4
     cuda = False
+    batch_size = 16
     snapshot_dir = 'snapshot'
     if not exists(snapshot_dir):
         makedirs(snapshot_dir)
@@ -32,36 +34,55 @@ def train(argv=sys.argv[1:]):
     training_data_loader = DataLoader(
         dataset=train_set,
         num_workers=4,
-        batch_size=16,
+        batch_size=batch_size,
         shuffle=True)
 
     validation_data_loader = DataLoader(
         dataset=val_set,
         num_workers=4,
-        batch_size=16,
+        batch_size=batch_size,
         shuffle=False)
+
     gennet_opts = dict()
     gennet_opts['upscale_factor'] = upscale_factor
     gennet = GenNet(net_opts=gennet_opts).train()
+    vgg = vgg13_52()
+    # for param in vgg.parameters():
+    #     param.requires_grad = False
+    disnet = DisNet().train()
+
+
     start_epoch = 1
-    criterion = nn.MSELoss()
+    content_criterion = nn.MSELoss()
+    adversarial_criterion = nn.BCELoss()
+
+    label = Variable(torch.FloatTensor(batch_size))
+    real_label = 1
+    fake_label = 0
+
     if cuda:
         net = net.cuda()
         criterion = criterion.cuda()
-    optimizer = Adam(gennet.parameters(), lr=0.0001)
+
+    optimizerG = Adam(gennet.parameters(), lr=0.0001)
+    optimizerD = Adam(disnet.parameters(), lr=0.0001)
 
     plots = Plots(nrows=1, ncols=2)
     plots.add_line(rind=1, cind=1)
     plots.set_title("Average PSNR", rind=1, cind=1)
-    plots.set_title("Objective", rind=1, cind=2)
     plots.set_xlabel("epoch", rind=1, cind=1)
-    plots.set_xlabel("epoch", rind=1, cind=2)
-    plots.add_line(rind=1, cind=2)
     plots.set_legend(['train', 'val'], rind=1, cind=1)
-    plots.set_legend(['train', 'val'], rind=1, cind=2)
+
+    plots.add_line(rind=1, cind=2)
+    plots.add_line(rind=1, cind=2)
+    plots.add_line(rind=1, cind=2)
+    plots.set_title("Objective", rind=1, cind=2)
+    plots.set_xlabel("epoch", rind=1, cind=2)
+    plots.set_legend(['train_g', 'train_d', 'val_g', 'val_d'], rind=1, cind=2)
 
     def train_epoch(epoch):
-        epoch_loss = 0
+        epoch_loss_g = 0
+        epoch_loss_d = 0
         total_psnr = 0
         for i, batch in enumerate(training_data_loader, 1):
             target = Variable(batch[0])
@@ -69,44 +90,106 @@ def train(argv=sys.argv[1:]):
             if cuda:
                 target = target.cuda()
                 input = input.cuda()
-            optimizer.zero_grad()
-            loss = criterion(gennet(input), target)
-            epoch_loss += loss.data[0]
-            total_psnr += psnr(loss.data[0])
+            
+            # Train Generator
+            optimizerG.zero_grad()
+            reconstructed = gennet(input)
+
+            feature_r = vgg(reconstructed)
+            feature_t = vgg(target)
+
+            content_loss = content_criterion(feature_r, feature_t.detach())
+
+            label.data.fill_(fake_label)
+            adversarial_loss = adversarial_criterion(disnet(reconstructed), label)
+
+            loss = content_loss + 1e-3*adversarial_loss
             loss.backward()
-            optimizer.step()
-            print("Epoch[{}]({}/{}): Loss: {:.4f}".format(
-                epoch, i, len(training_data_loader), loss.data[0]))
-        avg_loss = epoch_loss / len(training_data_loader)
+            optimizerG.step()
+
+            # Train Discriminator
+            optimizerD.zero_grad()
+            reconstructed = gennet(input)
+
+            fake_loss = adversarial_criterion(disnet(reconstructed.detach()), label)
+            fake_loss.backward()
+
+            label.data.fill_(real_label)
+            real_loss = adversarial_criterion(disnet(target), label)
+            real_loss.backward()
+            optimizerD.step()
+
+            epoch_loss_g += loss.data[0]
+            epoch_loss_d += fake_loss.data[0] + real_loss.data[0]
+            total_psnr += psnr(content_criterion(reconstructed, target).data[0])
+
+            print("Epoch[{}]({}/{}): Loss(G): {:.4f} Loss(D): {:.4f}".format(
+                epoch, i, len(training_data_loader), loss.data[0], fake_loss.data[0] + real_loss.data[0]))
+        avg_loss_g = epoch_loss_g / len(training_data_loader)
+        avg_loss_d = epoch_loss_d / len(training_data_loader)
         avg_psnr = total_psnr / len(training_data_loader)
-        print("Epoch {} : Avg Loss: {:.4f}".format(
-            epoch, avg_loss))
+
+        print("Epoch {} : Avg Loss(G): {:.4f} Avg Loss(D): {:.4f}".format(
+            epoch, avg_loss_g, avg_loss_d))
         plots.update([epoch, avg_psnr], rind=1, cind=1, lind=1)
-        plots.update([epoch, avg_loss], rind=1, cind=2, lind=1)
+        plots.update([epoch, avg_loss_g], rind=1, cind=2, lind=1)
+        plots.update([epoch, avg_loss_d], rind=1, cind=2, lind=2)
         plots.save()
 
     def validate(epoch):
-        total_loss = 0
+        epoch_loss_g = 0
+        epoch_loss_d = 0
         total_psnr = 0
-        for batch in validation_data_loader:
+        for i, batch in enumerate(validation_data_loader, 1):
             target = Variable(batch[0])
             input = Variable(batch[1])
             if cuda:
                 target = target.cuda()
                 input = input.cuda()
-            mse = criterion(gennet(input), target)
-            total_loss += mse.data[0]
-            total_psnr += psnr(mse.data[0])
-        avg_loss = total_loss / len(validation_data_loader)
-        avg_psnr = total_psnr / len(validation_data_loader)
-        print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr))
+            
+            reconstructed = gennet(input)
+
+            feature_r = vgg(reconstructed)
+            feature_t = vgg(reconstructed)
+
+            content_loss = content_criterion(feature_r, feature_t)
+
+            label.data.fill_(fake_label)
+            adversarial_loss = adversarial_criterion(disnet(reconstructed), label)
+
+            loss = content_loss + 1e-3*adversarial_loss
+
+            # Train Discriminator
+            reconstructed = gennet(input)
+
+            fake_loss = adversarial_criterion(disnet(reconstructed.detach()), label)
+
+            label.data.fill_(real_label)
+            real_loss = adversarial_criterion(disnet(target), label)
+
+            epoch_loss_g += loss.data[0]
+            epoch_loss_d += fake_loss.data[0] + real_loss.data[0]
+            total_psnr += psnr(content_criterion(reconstructed, target).data[0])
+
+            print("Epoch[{}]({}/{}): Loss(G): {:.4f} Loss(D): {:.4f}".format(
+                epoch, i, len(training_data_loader), loss.data[0], fake_loss.data[0] + real_loss.data[0]))
+        avg_loss_g = epoch_loss_g / len(training_data_loader)
+        avg_loss_d = epoch_loss_d / len(training_data_loader)
+        avg_psnr = total_psnr / len(training_data_loader)
+
+        print("Epoch {} : Avg Loss(G): {:.4f} Avg Loss(D): {:.4f}".format(
+            epoch, avg_loss_g, avg_loss_d))
         plots.update([epoch, avg_psnr], rind=1, cind=1, lind=2)
-        plots.update([epoch, avg_loss], rind=1, cind=2, lind=2)
+        plots.update([epoch, avg_loss_g], rind=1, cind=2, lind=3)
+        plots.update([epoch, avg_loss_d], rind=1, cind=2, lind=4)
         plots.save()
 
     def checkpoint(epoch):
-        path = join(snapshot_dir, "net-epoch-{}.pth".format(epoch))
+        path = join(snapshot_dir, "gnet-epoch-{}.pth".format(epoch))
         torch.save(gennet, path)
+        path = join(snapshot_dir, "dnet-epoch-{}.pth".format(epoch))
+        torch.save(disnet, path)
+
 
     for epoch in range(start_epoch, 10+1):
         train_epoch(epoch)
